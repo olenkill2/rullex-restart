f23ee34142ce73b22de95f6542129c9b363b904b
Stas Sokolov <olenkill@odva.pro>
2020-07-07 22:51:04 +0300

    fixes

--- site/ecosystem.config.js
+++ site/ecosystem.config.js
@@ -3,39 +3,9 @@
 module.exports = {
   apps: [
     {
-      name: "dev_api",
-      script: "index.js",
-      env: {
-        "NODE_ENV": "production",
-      },
-      env_master: {
-        "NODE_ENV": "production",
-      },
-      env_develop: {
-        "NODE_ENV": "production",
-        "ENV": "develop"
-      },
-      instances: "1",
-    },
-    {
       name: "dev_site",
       script: "npm",
       args: "run start",
-      env: {
-        "NODE_ENV": "production",
-      },
-      env_master: {
-        "NODE_ENV": "production",
-      },
-      env_develop: {
-        "NODE_ENV": "production",
-        "ENV": "develop"
-      },
-      instances: "1",
-    },
-    {
-      name: "prod_api",
-      script: "index.js",
       env: {
         "NODE_ENV": "production",
       },
